46af61a81a6b209bcbc1f5eb1ba70a77b3f3a185
Mauro Doneda <mauro.doneda@gmail.com>
2019-11-01 10:38:33 +0000

    Add project files.

---
+++ WorkersContracts/Entities/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkersContracts.Entities.Enums;
+
+namespace WorkersContracts.Entities
+
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+        
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
